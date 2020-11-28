ebbadaad16d0d920a3a4ee50f31b9c9349f3f0d8
Roman Kulich <rpkulich@gmail.com>
2020-11-28 19:41:43 +0100

    Update cve_list.py

--- cve_list.py
+++ cve_list.py
@@ -5,11 +5,14 @@
 import requests
 TGREEN =  '\033[32m'
 TWHITE = '\033[37m'
+TRED = '\033[31m'
 
 response = requests.get('http://cve.circl.lu/api/last')
 json = response.json()
 
+print("Last CVEs:")
 for show in json:
     print(TGREEN + "CVE: " + show['id'],TWHITE)
     print(TGREEN + "Summary info: ",TWHITE + show['summary'])
+    print(TGREEN + "Reference: ",TWHITE + show['references'][0])    
     print("")
